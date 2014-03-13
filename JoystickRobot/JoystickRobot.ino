234b22091681a278a4b0255725826804383b6f0b
Mags12998 <maggierobi@gmail.com>
2014-03-13 12:54:35 -0700

    Added decoding for the 4-packet structure
    *A/B button control
    *Reduced delay
    *Pos/neg for joystick
    *Potentiometer (throttle) added

--- JoystickRobot/JoystickRobot.ino
+++ JoystickRobot/JoystickRobot.ino
@@ -3,6 +3,9 @@
 #include <nRF24L01.h>
 #include <MirfHardwareSpiDriver.h>
 
+boolean a, b, j;
+boolean ud, rl;
+
 void setup(){
   Serial.begin(9600);
   Mirf.spi = &MirfHardwareSpi;
@@ -26,6 +29,7 @@ void loop(){
   while(Mirf.isSending()){
   }
   Serial.println("Finished sending");
+  Serial.println();
   delay(10);
   while(!Mirf.dataReady()){
     //Serial.println("Waiting");
@@ -46,8 +50,41 @@ void loop(){
   }
   Serial.println();
   
-  delay(1000);
+  decodeByte0(data[0]);
+  Serial.print("RL: ");
+  Serial.print(rl);
+  Serial.print("   UD: ");
+  Serial.print(ud);
+  Serial.print("   Joybtn: ");
+  Serial.print(j);
+  Serial.print("   B: ");
+  Serial.print(b);
+  Serial.print("   A: ");
+  Serial.print(a);
+  Serial.println();
+  
+  Serial.print("U/D: ");
+  Serial.print(getJoystickValue(data[1], ud));
+  Serial.print("   R/L: ");
+  Serial.print(getJoystickValue(data[2], rl));
+  Serial.print("   Pot: ");
+  Serial.print(data[3]);
+  Serial.println();
+  
+  delay(400);
 } 
-  
-  
-  
+
+void decodeByte0(byte raw) {
+  rl = raw / 16;
+  ud = (raw % 16) / 8;
+  j = (raw % 8) / 4;
+  b = (raw % 4) / 2;
+  a = (raw % 2);
+}
+
+int getJoystickValue(byte mag, boolean sign) {
+  if(sign)
+    return mag;
+  else
+    return mag * -1;
+}
