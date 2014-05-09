b9687cb4b4fa086e3ed36e17a394cc528eaccf2f
Sarang Joshi <sarangj@msn.com>
2014-05-09 12:30:23 -0700

    Smoothed algorithm

---
+++ RadioRollingRobotNew/RadioRollingRobotNew.ino
@@ -0,0 +1,99 @@
+#include <BFFRadioReceiver.h>
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+#include <Servo.h>
+
+boolean ButtonAPressed = false;
+boolean ButtonBPressed = false;
+boolean ButtonJoystickPressed = false;
+boolean RadioInContact = false;
+byte Throttle = 127;
+int JoystickY = 0;
+int JoystickX = 0;
+
+#define RadioMISO 12
+#define RadioMOSI 11
+#define RadioSCK 13
+#define RadioCE 8
+#define RadioCSN 7
+
+#define ServoPinL 9
+#define ServoPinR 10
+Servo servoL, servoR;
+
+BFFRadioReceiver radio(RadioMISO, RadioMOSI, RadioSCK, RadioCE, RadioCSN);
+
+void setup(){
+  Serial.begin(9600);
+  radio.init(&ButtonAPressed, &ButtonBPressed, &ButtonJoystickPressed, &RadioInContact,
+             &Throttle, &JoystickY, &JoystickX);
+  
+  servoL.attach(ServoPinL, 1000, 2000);
+  servoR.attach(ServoPinR, 1000, 2000);
+  
+  Serial.println("Beginning ... "); 
+}
+
+void loop(){
+  int rad = radio.update();
+  /*Serial.print(rad);
+  
+  Serial.print(" || Throttle: ");
+  Serial.print(Throttle);
+  Serial.print(" X: ");
+  Serial.print(JoystickX);
+  Serial.print(" Y: ");
+  Serial.println(JoystickY); */
+  
+  /*int modL = 1;
+  int modR = 1;
+  if(JoystickX > 50)
+    modR = -1;
+  if(JoystickX < -50)
+    modL = -1;
+    
+  Serial.print("modL: ");
+  Serial.print(modL);
+  Serial.print("modR: ");
+  Serial.println(modR);
+  
+  int valL = map(JoystickY, -255, 255, -500, 500) * modL;
+  int valR = map(JoystickY, -255, 255, -500, 500) * modR;*/
+  
+  // R IS REVERSED.
+  
+int valL = 0, valR = 0;
+		double k = 2.773;
+
+		valL = (int) ((k / sqrt(2)) * (JoystickX + JoystickY));
+		valR = (int) ((k / sqrt(2)) * (JoystickX - JoystickY));
+		
+		if(valL > 500) valL = 500;
+		if(valR > 500) valR = 500;
+		
+  /*
+  if(JoystickY > 50)
+    valL = valR = 500;
+  if(JoystickY < -50)
+    valL = valR = -500;
+  if(JoystickX > 50) {
+    valL = 500;
+    valR = -500;
+  }
+  if(JoystickX < -50) {
+    valL = -500;
+    valR = 500;
+  }*/
+    
+  servoL.writeMicroseconds(valL + 1500);
+  servoR.writeMicroseconds(valR + 1500);
+  
+  Serial.print("L: ");
+  Serial.print(valL);
+  Serial.print(", R: ");
+  Serial.println(valR);
+  
+  //delay(200);
+ }
