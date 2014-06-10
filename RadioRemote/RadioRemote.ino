a30d97233ab065d05f4336c383b389101f4926e9
Sarang Joshi <sarangj@msn.com>
2014-06-10 14:24:00 -0700

    Update RadioRemote.ino
    Updated analog input pins

--- RadioRemote/RadioRemote.ino
+++ RadioRemote/RadioRemote.ino
@@ -14,18 +14,17 @@ int ud, rl;
 int isUp, isRight;
 int throttle;
 
-#define throttlePot A4
-
+#define yAnalog A5
 #define xAnalog A6
-#define yAnalog A7
+#define throttlePot A7
 
 #define jButton 15
 #define aButton 16
 #define bButton 17
 
-//CEpin is usually 8 but on the receiver board it's 7 for whatever reason
-#define radioCEpin 8
-#define radioCSNpin 7
+// CEpin is usually 8 but on the PCB's it's 7.
+#define radioCEpin 7
+#define radioCSNpin 8
 
 void setup(){
   Serial.begin(9600);
